76b1ff615c7e762606119db262dfdd7857228564
gjscode <96193999+gjscode@users.noreply.github.com>
2021-12-28 15:28:02 -0500

    added gravity

--- Platformer/src/Actors/Actor.gd
+++ Platformer/src/Actors/Actor.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
 class_name Actor
 
+export var gravity: = 3000.0
+
+var velocity: = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	var velocity: = Vector2(300, 0)
+	velocity.y += gravity * delta
 	move_and_slide(velocity)
