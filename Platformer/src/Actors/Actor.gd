6a68534b8dcdbf4a1806e3ace69d579103164afe
gjscode <96193999+gjscode@users.noreply.github.com>
2021-12-28 11:08:04 -0500

    started character movement

--- Platformer/src/Actors/Actor.gd
+++ Platformer/src/Actors/Actor.gd
@@ -1,3 +1,7 @@
-extends Node
+extends KinematicBody2D
 class_name Actor
 
+
+func _physics_process(delta: float) -> void:
+	var velocity: = Vector2(300, 0)
+	move_and_slide(velocity)
--- Platformer/src/Actors/Player.gd
+++ Platformer/src/Actors/Player.gd
@@ -1,2 +1,2 @@
-extends KinematicBody2D
+extends Actor
 
