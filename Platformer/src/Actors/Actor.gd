9cbefcddf0d6f8fd0b38449143b7be2baa7b9c7d
gjscode <96193999+gjscode@users.noreply.github.com>
2022-01-01 18:58:39 -0500

    Restart, the sequel (with notes)

--- Platformer/src/Actors/Actor.gd
+++ Platformer/src/Actors/Actor.gd
@@ -1,12 +1,22 @@
+#must share same type as node, or one of its child nodes
+#Player node is type KinematicBody2D
 extends KinematicBody2D
+#registers the script to be used
 class_name Actor
 
-export var speed: = Vector2(300.0, 1000.0)
+#export keyword makes this value configurable in the inspector on the right
 export var gravity: = 3000.0
 
+#Vector2() reprents 2d space (x, y)
 var velocity: = Vector2.ZERO
 
+#physics_process is a function which godot will call every frame of your game (30-60fps)
+#used for things that require physics (collisions, movement etc)
 func _physics_process(delta: float) -> void:
+	#adds gravity value to velocity's y axis
+	#delta is a value that represents the time since the last frame (makes code "frame rate independent")
+	#this means regardless of frame rate, the node will move the "distance" it is suppose to (whether smoothly or not)
 	velocity.y += gravity * delta
-	#velocity.y = min(speed.y, velocity.y)
-	velocity = move_and_slide(velocity)
+	#this will move the player based on the inputed value (which we created above)
+	#has delta value built in so no need to * velocity by delta
+	move_and_slide(velocity)
